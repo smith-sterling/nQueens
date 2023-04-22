bd4a8ea579c3c74ca98675e097979cbce1dcd15e
smith-sterling <104526450+Viper-Fist@users.noreply.github.com>
2023-04-21 22:16:49 -0600

    Fixed a quick bug in the racer timer output stuff, and also made it more clean...

--- src/Racer.java
+++ src/Racer.java
@@ -1,14 +1,37 @@
+import java.time.Duration;
 
 public class Racer {
 
     public static void main(String[] args) {
-        long time = System.currentTimeMillis();
+        long preSolve;
         for (int i = 4; i < 20; ++i) {
             Board board = new Board(i);
+            
+            preSolve = System.currentTimeMillis();
             board.solve();
-            System.out.printf("Here's with an %dx%d board, solved in %.3f seconds%n", i, i,
-                    ((double) System.currentTimeMillis() - time)/1000);
+            long postSolve = System.currentTimeMillis();
+            
+            System.out.printf("Here's with an %dx%d board, solved in %s%n", i, i, elapsedTime(preSolve, postSolve));
+            
             Main.cout(board.toString());
+            
+        }
+    }
+    
+    private static String elapsedTime(long preSolve, long postSolve) {
+        Duration duration = Duration.ofMillis(postSolve - preSolve);
+    
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+        long seconds = duration.getSeconds() % 60;
+        long millisPart = duration.toMillis() % 1000;
+    
+        if (hours > 0) {
+            return String.format("%d hours, %d minutes, %d.%03d seconds", hours, minutes, seconds, millisPart);
+        } else if (minutes > 0) {
+            return String.format("%d minutes, %d.%03d seconds", minutes, seconds, millisPart);
+        } else {
+            return String.format("%d.%03d seconds", seconds, millisPart);
         }
     }
 }
